47b23490a716d307e345bb973346853b3a75c5d6
Xbee4u <xforcebest@gmail.com>
2024-12-04 16:21:29 +0100

    Add files via upload

---
+++ Asses.js
@@ -0,0 +1,16 @@
+let person = {
+        "Full name": "Ogwutum Chiabuotu Destiny",
+        Age: 23,
+        "Application ID": "LT240119",
+        "Matric No": "24/2529",
+        School: "Computing",
+        Course: "Software Engineering",
+        "Phone No": +2349164821228,
+        Email: "xforcebest@gmail.com",
+        Nationality: "Nigerian",
+        Address: "HC 10, Enugu Estate, P.H. , Rivers State"
+        State of Origin: "Rivers State"
+      
+    
+    }
+console.log(person)
